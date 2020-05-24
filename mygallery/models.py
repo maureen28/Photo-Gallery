417dc4c1657bcb25073ff3b6676af245f1e45066
Maureen <murimimaureen69@gmail.com>
2020-05-24 22:50:34 +0300

    Add location model

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    country = models.CharField(max_length =150)
+
+    def __str__(self):
+        return self.country
+    class Meta:
+        ordering = ['country']
