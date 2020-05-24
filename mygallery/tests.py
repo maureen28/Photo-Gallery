418f1524e7b8746ffd025c5c4689259f8e07c02f
Maureen <murimimaureen69@gmail.com>
2020-05-24 22:59:54 +0300

    Add location test case, setup, instace, test and save method

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setup(self):
+        self.maureen = Location(country = 'Kenya')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.maureen, Location))
