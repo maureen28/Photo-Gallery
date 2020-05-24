ae6f21566176cbacb07c5ef20cde31758482930d
Maureen <murimimaureen69@gmail.com>
2020-05-24 23:57:03 +0300

    Add all images and a container to our index template

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from mygallery.models import Image,Category, Location
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html',{})
+    all_images = Image.objects.all()
+    all_categories = Category.objects.all()
+    all_locations = Location.objects.all()
+    return render(request, 'index.html',{'all_images': all_images, 'all_categories': all_categories, 'all_locations' : all_locations})
 
 def get_category(request):
     return render(request, 'index.html',{})
