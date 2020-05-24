f5b3bbc9b2b89fb26fa03fb526b530f9fa4c3011
Maureen <murimimaureen69@gmail.com>
2020-05-24 21:52:52 +0300

    Add our navbar

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mygallery.apps.MygalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
