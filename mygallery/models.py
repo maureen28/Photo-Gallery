7bb984d23484c508b3d0539d6fc3c09b72b71b8c
Maureen <murimimaureen69@gmail.com>
2020-05-24 22:55:10 +0300

    Add image model and foreign key for both category and location

--- mygallery/models.py
+++ mygallery/models.py
@@ -16,4 +16,12 @@ class Category(models.Model):
     def __str__(self):
             return self.title
 
+class Image(models.Model):
+    name = models.CharField(max_length = 80)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
 
