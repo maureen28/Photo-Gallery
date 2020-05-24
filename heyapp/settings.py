ea92f4b424042b4d716af508bb2a4c8d7d38c257
Maureen <murimimaureen69@gmail.com>
2020-05-24 21:38:50 +0300

    Add bootstrap

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mygallery'
+    'mygallery',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
